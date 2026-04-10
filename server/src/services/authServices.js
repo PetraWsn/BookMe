bd6d1f3ac99adc5ef092f839c1c257ae37e6fd59
Petra Wessén <petra.wessen@hotmail.com>
2026-04-10 16:01:50 +0200

    ny fix

--- server/src/services/authServices.js
+++ server/src/services/authServices.js
@@ -13,14 +13,14 @@ export const registerUser = async ({ name, email, password }) => {
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     logger.warn(`REGISTRERINGS_FÖRSÖK: E-post är redan registrerad.`);
-    // INBYGGT FEL
+    // HÄR BYGGER VI IN SÅRBARHETEN: Vi skickar med inmatningen i felet
     throw new Error(`Användaren med e-post ${email} finns redan!`);
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({ name, email, password: hashedPassword });
 
-  logger.info(`NY ANVÄNDARE: Registrering av nytt konto.`);
+  logger.info(`NY ANVÄNDARE: Registrerat ett konto.`);
   return { id: user._id, name: user.name, email: user.email };
 };
 
@@ -110,7 +110,7 @@ export const deleteAccount = async (userId) => {
   await redis.del(`refresh:${userId}`);
 
   logger.info(
-    `KONTO_RADERAT: En användare har raderat sitt konto och rensat sina refresh tokens.`,
+    `KONTO_RADERAT: Användare med ID: ${userId} raderade sitt konto och rensade sin refresh token.`,
   );
   return { message: "Kontot har raderats permanent" };
 };
